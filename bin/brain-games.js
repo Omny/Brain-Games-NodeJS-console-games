b5499c866498adeb37035e2d94603404f0ea7adc
Artem Petrishchev <artem.omny@gmail.com>
2022-05-23 19:46:21 +0300

    Add greeting vs readline-sync

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import greeting from "../index.js";
+
+console.log("Welcome to the Brain Games!");
+console.log(greeting());
---
+++ index.js
@@ -0,0 +1,3 @@
+import greeting from "./src/cli.js";
+
+export default greeting;
