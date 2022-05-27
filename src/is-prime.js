6641779581c3bdf8b2d837400b7c0caa2b04f565
Artem Petrishchev <artem.omny@gmail.com>
2022-05-27 23:25:45 +0300

    update isPrime function

--- src/is-prime.js
+++ src/is-prime.js
@@ -1,7 +1,13 @@
 function isPrime(num) {
+  if (num <= 1) return false;
   if (num === 2) return true;
-  if (num < 2 || num % 2 === 0) return false;
-  for (let i = 3; i * i <= num; i += 2) if (num % i === 0) return false;
+  const num2 = Math.sqrt(num); // num2 is the square root of num
+  for (let i = 2; i <= num2; i += 1) {
+    // note that we are working now with the square root
+    if (num2 % i === 0) {
+      return false;
+    }
+  }
   return true;
 }
 export default isPrime;
