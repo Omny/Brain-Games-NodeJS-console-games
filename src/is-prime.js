e8c326be7adc65c39e9f4b84c3a6bfe7ccdf4e81
Artem Petrishchev <artem.omny@gmail.com>
2022-05-28 10:10:34 +0300

    Replace prime function

--- src/is-prime.js
+++ src/is-prime.js
@@ -1,13 +1,9 @@
 function isPrime(num) {
-  if (num <= 1) return false;
-  if (num === 2) return true;
-  const num2 = Math.sqrt(num); // num2 is the square root of num
-  for (let i = 2; i <= num2; i += 1) {
-    // note that we are working now with the square root
-    if (num2 % i === 0) {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
       return false;
     }
   }
-  return true;
+  return num > 1;
 }
 export default isPrime;
